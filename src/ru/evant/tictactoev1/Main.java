69aa41df0b7a774748d1bc41856dc387098c0cca
Anton Evseev <eavae@mail.ru>
2021-04-18 19:48:28 +0500

    + Игровое поле + Отобразить игровое поле в консоли

--- src/ru/evant/tictactoev1/Main.java
+++ src/ru/evant/tictactoev1/Main.java
@@ -2,7 +2,30 @@ package ru.evant.tictactoev1;
 
 public class Main {
 
+    private static String [] gameField; // Игровое поле. Массив.
+
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Крестики - Нолики");
+        create(); // Создать игровое поле
+        display(); // Отобразить игровое поле в консоли
+
     }
-}
+
+    /* Отобразить игровое поле в консоли */
+    private static void display() {
+        for (int i = 1; i <= gameField.length; i++) {
+            if (i % 3 == 0) System.out.print(gameField[i - 1] + "\n");
+            else System.out.print(gameField[i - 1]);
+        }
+        System.out.println();
+    } // конец метода display()
+
+    /* Создать игровое поле */
+    private static void create() {
+        gameField = new String[9]; // Создать массив длинной 9. Это 9 ячеек для поля 3 на 3 ячейки.
+        for (int i = 1; i <= gameField.length; i++) {
+            gameField[i-1] = " _ "; // Заполнить массив начальными символами, обозначающими свободную ячейку.
+        }
+    } // конец метода create()
+} // конец класса Main
