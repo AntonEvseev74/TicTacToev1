06caaf4ea4def51e30355d330e5eafe1bb8619a7
Anton Evseev <eavae@mail.ru>
2021-04-18 22:11:06 +0500

    + Добавлены проверки пользовательского ввода на корректность + Добавлена проверка комбинаций выигрыша + Можно играть

--- src/ru/evant/tictactoev1/Main.java
+++ src/ru/evant/tictactoev1/Main.java
@@ -18,57 +18,113 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static String [] gameField; // Ссылочная переменная на массив. Для игрового поля. Массив.
-    private static Scanner scanner;     // Ссылочная переменная для получения пользовательского ввода.
-    public static final String X = " X ";    // Крестик
-    public static final String O = " O ";    // Нолик
-    private static boolean checkCell;   // Проверка ячейки. true - свободна, false - не свобона
-    private static boolean gameOver;    // Конец игры
+    private static String[] gameField;      // Ссылочная переменная на массив. Для игрового поля. Массив.
+    private static int input;               // Переменная для пользовательского ввода
+    public static final String X = " X ";   // Крестик
+    public static final String O = " O ";   // Нолик
+    private static boolean checkCell;       // Проверка ячейки. true - свободна, false - не свобона
+    private static boolean gameOver;        // Конец игры
 
     public static void main(String[] args) {
 
-        System.out.println("Крестики - Нолики");
+        System.out.println("Крестики - Нолики\nНомера ячеек:\n 1  2  3\n 4  5  6\n 7  8  9\nИгровое поле:");
         create(); // Создать игровое поле
         display(); // Отобразить игровое поле в консоли
 
-        game(); // Ход игры. Ходы игроков. Вывод в консоль.
-        game();
+        while (!gameOver) {
+            game(); // Ход игры. Ходы игроков. Вывод в консоль.
+        }
 
-    }
+    }  // конец метода main()
 
     /* Ход игры */
     private static void game() {
-        moveUser(); // Ход пользователя
-        display();  // Отобразить поле
-        moveAI();   // Ход компьютера
-        display();  // Отобразить поле
-    }
+        if (!gameOver) {
+            moveUser(); // Ход пользователя
+            display();  // Отобразить поле
+        }
+        if (!gameOver) {
+            moveAI();   // Ход компьютера
+            display();  // Отобразить поле
+        }
+    }  // конец метода game()
 
+    /* Проверка выигрышных комбинаций, три в ряд */
+    private static void checkGameOver() {
+        if (gameField[0].equals(X) && gameField[1].equals(X) && gameField[2].equals(X)) gameOver = true;
+        if (gameField[0].equals(O) && gameField[1].equals(O) && gameField[2].equals(O)) gameOver = true;
+
+        if (gameField[3].equals(X) && gameField[4].equals(X) && gameField[5].equals(X)) gameOver = true;
+        if (gameField[3].equals(O) && gameField[4].equals(O) && gameField[5].equals(O)) gameOver = true;
+
+        if (gameField[6].equals(X) && gameField[7].equals(X) && gameField[8].equals(X)) gameOver = true;
+        if (gameField[6].equals(O) && gameField[7].equals(O) && gameField[8].equals(O)) gameOver = true;
+
+        if (gameField[0].equals(X) && gameField[3].equals(X) && gameField[6].equals(X)) gameOver = true;
+        if (gameField[0].equals(O) && gameField[3].equals(O) && gameField[6].equals(O)) gameOver = true;
+
+        if (gameField[1].equals(X) && gameField[4].equals(X) && gameField[7].equals(X)) gameOver = true;
+        if (gameField[1].equals(O) && gameField[4].equals(O) && gameField[7].equals(O)) gameOver = true;
+
+        if (gameField[2].equals(X) && gameField[5].equals(X) && gameField[8].equals(X)) gameOver = true;
+        if (gameField[2].equals(O) && gameField[5].equals(O) && gameField[8].equals(O)) gameOver = true;
+
+        if (gameField[0].equals(X) && gameField[4].equals(X) && gameField[8].equals(X)) gameOver = true;
+        if (gameField[0].equals(O) && gameField[4].equals(O) && gameField[8].equals(O)) gameOver = true;
+
+        if (gameField[2].equals(X) && gameField[4].equals(X) && gameField[6].equals(X)) gameOver = true;
+        if (gameField[2].equals(O) && gameField[4].equals(O) && gameField[6].equals(O)) gameOver = true;
+    } // конец метода checkGameOver()
+
+    /* Ход компьютера */
     private static void moveAI() {
-        int randomNumber = (int) (Math.random() * 8); // Присвоить пременной случайное число от 0 до 8. Это индексы ячеек игрового поля.
-        checkCell = checkCell(randomNumber); // Присвоить переменной значение true - свободная ячейка или false - не свободная ячейка
+        checkGameOver();
+        if (!gameOver) {
+            int randomNumber = (int) (Math.random() * 8); // Присвоить пременной случайное число от 0 до 8. Это индексы ячеек игрового поля.
+            checkCell = checkCell(randomNumber); // Присвоить переменной значение true - свободная ячейка или false - не свободная ячейка
 
-        if (checkCell) gameField[randomNumber] = O; // Если свободна (true). Присвоить ячейке игрового поля значение нолик
-        else moveAI(); // Иначе сново запустить ход компьютера
-    }
+            if (checkCell)
+                gameField[randomNumber] = O; // Если свободна (true). Присвоить ячейке игрового поля значение нолик
+            else moveAI(); // Иначе сново запустить ход компьютера
+        } else {
+            System.out.println("Вы выиграли! Конец игры");
+        }
+    } // конец метода moveAI()
 
     /* Ход пользователя */
     private static void moveUser() {
-        scanner = new Scanner(System.in);
-        System.out.print("Выберите ячейку(введите число от 1 до 9):\n$:/ ");
-        int input = scanner.nextInt(); // Присвоить переменной input значение введенное пользователем
-        input = input - 1;
-        checkCell = checkCell(input); // Присвоить переменной значение true - свободная ячейка или false - не свободная ячейка
+        checkGameOver(); // Проверка окончена ли игра
+        if (!gameOver) { // если игра не окончена
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Выберите ячейку(введите число от 1 до 9):\n$:/ ");
+            if (scanner.hasNextInt()){ // Проверить, является ли ввод пользователя - целым числом
+                input = scanner.nextInt(); // Присвоить переменной input значение введенное пользователем
+                checkInput(input); // проверить число в диапазоне от 1 до 9
+                int indexCell = input - 1; //
+                checkCell = checkCell(indexCell); // Присвоить переменной значение true - свободная ячейка или false - не свободная ячейка
 
-        if (checkCell) gameField[input] = X; // Если свободна (true). Присвоить ячейке игрового поля значение крестик
-        else moveUser(); // Иначе сново запустить ход пользователя
-    }
+                if (checkCell) // если ячейка свободна
+                    gameField[indexCell] = X; // Если свободна (true). Присвоить ячейке игрового поля значение крестик
+                else moveUser(); // Иначе сново запустить ход пользователя
+            } else {
+                moveUser();
+            }
+        } else {
+            System.out.println("Выиграл компьютер! Конец игры");
+        }
+    } // конец метода moveUser()
+
+    /* Проверка пользовательского ввода на корректность */
+    private static void checkInput(int userInput) {
+        if(userInput > 0 && userInput <= 9) input = userInput;
+        else moveUser();
+    } // конец метода checkInput()
 
     /* Проверка ячейки. Свободна или нет. */
     private static boolean checkCell(int indexCell) {
         if (gameField[indexCell].equals(X) || gameField[indexCell].equals(O)) return false;
         else return true;
-    }
+    } // конец метода checkCell()
 
     /* Отобразить игровое поле в консоли */
     private static void display() {
@@ -83,7 +139,7 @@ public class Main {
     private static void create() {
         gameField = new String[9]; // Создать массив длинной 9. Это 9 ячеек для поля 3 на 3 ячейки.
         for (int i = 1; i <= gameField.length; i++) {
-            gameField[i-1] = " _ "; // Заполнить массив начальными символами, обозначающими свободную ячейку.
+            gameField[i - 1] = " _ "; // Заполнить массив начальными символами, обозначающими свободную ячейку.
         }
     } // конец метода create()
 } // конец класса Main
