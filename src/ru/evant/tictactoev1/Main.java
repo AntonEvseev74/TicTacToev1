7a8f0f8c5d3b0339aad36339751eae059289f0a6
Anton Evseev <eavae@mail.ru>
2021-04-18 20:45:05 +0500

    + Проверка ячейки. Свободна или нет.  + Исправление логики ходов игроков

--- src/ru/evant/tictactoev1/Main.java
+++ src/ru/evant/tictactoev1/Main.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 
 /* Игра Крестики - Нолики */
 /* Игровое поле:
-    _ _ _  1 2 3
-    _ _ _  4 5 6
-    _ _ _  7 8 9
+    _ _ _
+    _ _ _
+    _ _ _
+    Индексы ячеек игрового поля:
+    1 2 3
+    4 5 6
+    7 8 9
+    Символы игроков:
+    X - крестик
+    O - нолик
  */
 
 public class Main {
 
     private static String [] gameField; // Ссылочная переменная на массив. Для игрового поля. Массив.
     private static Scanner scanner;     // Ссылочная переменная для получения пользовательского ввода.
+    public static final String X = " X ";    // Крестик
+    public static final String O = " O ";    // Нолик
+    private static boolean checkCell;   // Проверка ячейки. true - свободна, false - не свобона
+    private static boolean gameOver;    // Конец игры
 
     public static void main(String[] args) {
 
@@ -21,6 +32,7 @@ public class Main {
         display(); // Отобразить игровое поле в консоли
 
         game(); // Ход игры. Ходы игроков. Вывод в консоль.
+        game();
 
     }
 
@@ -34,16 +46,28 @@ public class Main {
 
     private static void moveAI() {
         int randomNumber = (int) (Math.random() * 8); // Присвоить пременной случайное число от 0 до 8. Это индексы ячеек игрового поля.
-        gameField[randomNumber] = " O "; // Присвоить ячейке игрового поля значение нолик
+        checkCell = checkCell(randomNumber); // Присвоить переменной значение true - свободная ячейка или false - не свободная ячейка
+
+        if (checkCell) gameField[randomNumber] = O; // Если свободна (true). Присвоить ячейке игрового поля значение нолик
+        else moveAI(); // Иначе сново запустить ход компьютера
     }
 
     /* Ход пользователя */
     private static void moveUser() {
         scanner = new Scanner(System.in);
-        System.out.println("Выберите ячейку(введите число от 1 до 9):\n$:/ ");
+        System.out.print("Выберите ячейку(введите число от 1 до 9):\n$:/ ");
         int input = scanner.nextInt(); // Присвоить переменной input значение введенное пользователем
         input = input - 1;
-        gameField[input] = " X "; // Присвоить ячейке игрового поля значение крестик
+        checkCell = checkCell(input); // Присвоить переменной значение true - свободная ячейка или false - не свободная ячейка
+
+        if (checkCell) gameField[input] = X; // Если свободна (true). Присвоить ячейке игрового поля значение крестик
+        else moveUser(); // Иначе сново запустить ход пользователя
+    }
+
+    /* Проверка ячейки. Свободна или нет. */
+    private static boolean checkCell(int indexCell) {
+        if (gameField[indexCell].equals(X) || gameField[indexCell].equals(O)) return false;
+        else return true;
     }
 
     /* Отобразить игровое поле в консоли */
